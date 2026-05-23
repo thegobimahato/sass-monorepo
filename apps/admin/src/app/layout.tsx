c245d147d1e83ace6d0065c54058f16d9cf21c45
Gobi Mahato <thegobimahato@gmail.com>
2026-05-23 10:04:26 +0530

    chore: set up shadcn/ui as shared package

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import "@repo/ui/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@repo/ui/components/button";
+
 export default function Home() {
   return (
     <div className="p-4">
       <h1 className="text-4xl">Dashboard</h1>
+      <Button>Button</Button>
     </div>
   );
 }
